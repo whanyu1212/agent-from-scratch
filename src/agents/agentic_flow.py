5d268e8a283b786e2e3a4d782ffe80433d07dfa2
Hy <107110503+whanyu1212@users.noreply.github.com>
2024-12-20 14:41:45 +0000

    clean up and modified function names to be more intuitive and explicit

--- src/agents/agentic_flow.py
+++ src/agents/agentic_flow.py
@@ -2,6 +2,7 @@ import sys
 
 sys.path.insert(0, "..")
 from colorama import init, Fore, Style
+from typing import Any
 from loguru import logger
 from src.prompts.prompt import agent_system_prompt_template
 from src.models.gemini_models import GeminiModel
@@ -20,69 +21,66 @@ init()
 
 
 class Agent:
-    def __init__(self, tools, model_service, model_name):
-        """
-        Initializes the agent with a list of tools and a model.
+    def __init__(self, tools: list, model_name: str):
+        """Initializes an Agent instance.
 
-        Parameters:
-        tools (list): List of custom functions as tools.
-        model_service (class): The model service class with a generate_text method.
-        model_name (str): The name of the model to use.
+        Args:
+            tools (list): a list of tool functions that the agent can use.
+            model_name (str): the model name available in the Gemini API.
         """
         self.tools = tools
-        self.model_service = GeminiModel
         self.model_name = model_name
 
-    def prepare_tools(self):
-        """
-        Stores the tools in the toolbox and returns their descriptions.
+    def format_tool_descriptions(self) -> str:
+        """Prepares the tool descriptions for the system prompt.
 
         Returns:
-        str: Descriptions of the tools stored in the toolbox.
+            str: The tool names and descriptions as a string.
         """
         toolbox = ToolBox()
         toolbox.register_functions(self.tools)
         tool_descriptions = toolbox.get_registered_functions_as_string()
         return tool_descriptions
 
-    def think(self, prompt):
+    def plan_action(self, prompt: str) -> dict:
         """
-        Runs the generate_text method on the model using the system prompt template and tool descriptions.
+        Analyzes the user prompt and plans the appropriate tool execution.
 
         Parameters:
-        prompt (str): The user query to generate a response for.
+        prompt (str): The user query to analyze.
 
         Returns:
-        dict: The response from the model as a dictionary.
+        dict: Contains selected tool and arguments for execution.
         """
-        tool_descriptions = self.prepare_tools()
+        tool_descriptions = self.format_tool_descriptions()
         agent_system_prompt = agent_system_prompt_template.format(
             tool_descriptions=tool_descriptions
         )
 
         logger.info(
-            f"{Fore.CYAN}System prompt: {agent_system_prompt} {Style.RESET_ALL}"
+            f"{Fore.CYAN}System prompt:\n {agent_system_prompt} {Style.RESET_ALL}"
         )
 
         model_instance = GeminiModel(
             model_name=self.model_name, system_prompt=agent_system_prompt, temperature=0
         )
 
-        # Generate and return the response dictionary
-        agent_response_dict = model_instance.generate_text(prompt)
-        return agent_response_dict
+        # Generate and return the execution plan
+        action_plan = model_instance.generate_text(prompt)
+        return action_plan
 
-    def execute(self, prompt):
+    def execute_planned_action(self, prompt: str) -> Any:
         """
-        Parses the dictionary returned from think and executes the appropriate tool.
+        Executes the planned action by routing to the appropriate tool.
 
         Parameters:
-        prompt (str): The user query to generate a response for.
+        prompt (str): The user query to execute.
 
         Returns:
-        The response from executing the appropriate tool or the tool_input if no matching tool is found.
+        Any: The response from executing the selected tool.
         """
-        agent_response_dict = self.think(prompt)
+        agent_response_dict = self.plan_action(prompt)
+        print(f"{Fore.BLUE}Tool Routing: {agent_response_dict}{Style.RESET_ALL}\n")
         tool_choice = agent_response_dict.get("tool_choice")
         tool_input = agent_response_dict.get("tool_input")
 
@@ -91,17 +89,15 @@ class Agent:
                 response = tool(tool_input)
                 print(f"{Fore.GREEN}Input argument: {tool_input}{Style.RESET_ALL}\n")
                 print(f"{Fore.GREEN}Response: {response}{Style.RESET_ALL}\n")
-                return
-        return
+                return response
+        return None
 
 
 if __name__ == "__main__":
 
     tools = [basic_calculator, parse_datetime]
 
-    agent = Agent(
-        tools=tools, model_service=GeminiModel, model_name="gemini-2.0-flash-exp"
-    )
+    agent = Agent(tools=tools, model_name="gemini-2.0-flash-exp")
     prompt = "convert '2025-01-01' to a datetime object"
 
-    agent.execute(prompt)
+    agent.execute_planned_action(prompt)
